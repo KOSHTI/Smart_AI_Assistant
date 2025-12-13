243c303f7750f9d10af51ce8c0ba3f2ac740c831
Shubham Koshti <98097225+KOSHTI@users.noreply.github.com>
2025-12-13 18:27:27 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 // config.js
-const API_KEY = "AIzaSyDANkCsMnjpc5bmJ4jPnvPXncl6JSesvHM";  // Replace with your actual API key
+const API_KEY = "";  // Replace with your actual API key
+
 export default API_KEY;
